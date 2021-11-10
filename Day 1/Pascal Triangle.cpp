5d87c230610b905711f40c120647aa728f37dd71
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-11-10 11:36:45 +0530

    1

--- Day 1/Pascal Triangle.cpp
+++ Day 1/Pascal Triangle.cpp
@@ -1,5 +1,7 @@
 // link = https://takeuforward.org/data-structure/program-to-generate-pascals-triangle/
 
+// PRINT THE WHOLE PASCAL TRIANGLE =======================================================================================================================
+
 int main()
 {
     int rows = 5; // no. of pascal triangle rows
@@ -24,5 +26,39 @@ int main()
         cout<<endl;
     }
 
+    return 0;
+}
+
+// PRINT A SPECIFIC ROW  ==========================================================================================================================================
+
+// n -> no. of rows in the pascal triangle
+// row -> specific row no. to be printed
+void printPascalRow(int n, int row){
+    vector<vector<int>> r(n);
+    
+    for(int i = 0; i<n; i++){
+        r[i].resize(i+1);
+        r[i][0] = r[i][i] = 1;
+        
+        for(int j = 1; j<i; j++){
+            r[i][j] = r[i-1][j-1] + r[i-1][j]; 
+        }
+        
+        // print the specific row and then break
+        if(i == row-1){
+            for(int k=0; k<=i; k++){
+                cout<<r[i][k]<<" ";
+            }
+            break;
+        }
+    }
+}
+
+int main()
+{
+    int n = 5;
+    
+    printPascalRow(n, 4);
+    
     return 0;
 }
