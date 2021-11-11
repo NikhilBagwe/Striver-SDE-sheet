745b0659751e4c2611bd4db188266167164d54b3
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-11-11 16:35:25 +0530

    1

--- Day 2/Find the duplicate number.cpp
+++ Day 2/Find the duplicate number.cpp
@@ -41,3 +41,41 @@ int main()
     
     return 0;
 }
+
+// BETTER ================ Floyd's Tortoise and Hare (Cycle Detection) ========================= TC = O(n) =======================================================
+
+/*
+1. Init 2 pointers - fast and slow
+2. Move the 'slow' ptr at speed of '1' and fast by '2' until they collide.
+3. When 1st collision takes place i.e slow == fast, come out of loop
+4. Take fast ptr back to the start of array 
+5. Move both the pointers by 1 to find the duplicate no.
+*/
+
+int main()
+{
+    vector<int> nums {2, 5, 8, 6, 8, 3, 8, 9, 7, 1};
+    int n = nums.size();
+     
+    int slow = nums[0];
+    int fast = nums[0];
+    
+    do{
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    }while(slow != fast);
+    
+    fast = nums[0];
+    
+//     slow = nums[slow];
+//     fast = nums[fast];
+    
+    while(slow != fast){
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+    
+    cout<<slow;
+    
+    return 0;
+}
