2660b80068bbe76533abfe521b572163ba418c24
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-11-12 12:44:40 +0530

    1

--- Day 2/Find the repeating and missing numbers.cpp
+++ Day 2/Find the repeating and missing numbers.cpp
@@ -31,3 +31,31 @@ int main()
     
     return 0;
 }
+
+// OPTIMAL ================================== XOR =========== TC = O(n) =============================================================================================
+
+/*
+LOGIC:
+
+eg: a[] = {4, 3, 6, 2, 1, 1}
+
+1. XOR all the elements of array and store the result in xor1 variable. In our eg, its xor1=3
+2. Now, take XOR of elements from 1 to n with xor1. In our eg, its xor1=4.
+3. Now the '4' that we got is nothing but result of (X^Y) where X = missing no., Y = repeating no.
+4. When you perform, 
+    (xor of all array elements) ^ (xor of elements from 1 to n) = (4^3^6^2^1^1) ^ (1^2^3^4^5^6) 
+   on cancelling terms you will get '5' and '1' which are the required no.s 
+5. Next we have to find the bit that is set in '4' i.e binary of 4 = '100'
+6. So for '4' the rightmost bit is set. That bit is set, means either X is 0 or 1, OR Y is 0 or 1(from point 3), 
+   since only different bits on doing XOR gives '1'.
+7. Separate the elements form array into 2 buckets:
+   bucket 1 = contains elements whose 1st bit from rightmost is set.
+   bucket 2 = contains elements whose 1st bit from rightmost is not set
+8. After iterating the array using set_bit:
+   bucket 1 = 4, 6
+   bucket 2 = 3, 2, 1, 1
+9. Again iterate from 1 to n using set_bit:
+   bucket 1 = 4, 6 | 4, 5, 6
+   bucket 2 = 3, 2, 1, 1 | 1, 2, 3 
+   On cancellation, you will get 5 and 1 as answer.
+*/
