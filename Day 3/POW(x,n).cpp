9d40b3b01864a7fb2278f589e47fa00f6ce3e2db
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-11-15 13:26:37 +0530

    1

---
+++ Day 3/POW(x,n).cpp
@@ -0,0 +1,33 @@
+// BRUTE ===================================================================== TC = O(n) =====================================================================
+
+// This method fails when you try to convert '-2,147,483,648' to abs(-2,147,483,648) as it overflows because the positive range is '2,147,483,647'
+
+int main()
+{
+    double x = 2.00000;
+    int n = -4, n1 = 0, flag = 0;
+    double ans = 2.00000;
+    
+    // Checking whether the 'n' is a negative or positive no.
+    if(n < 0){
+        // Consider the absolute value of 'n'
+        n1 = abs(n);
+        flag = 1;
+    }
+    else n1 = n;
+    
+    // Calculate the x^n1 
+    for(int i = 0; i<n1-1; i++){
+        ans *= x;
+    }
+    
+    if(flag == 0){
+        cout<<fixed<<setprecision(4)<<ans;
+    }
+    // If 'n' was negative then divide the 'ans' from 1 as "x^(-n) = 1 / x^n"
+    else{
+        cout<<fixed<<setprecision(4)<<1/ans;
+    }
+
+    return 0;
+}
