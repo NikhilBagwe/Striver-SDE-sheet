391bcedde24fc2f43f2154e82446ff096731bb3e
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-11-17 17:34:22 +0530

    1

--- Day 9 - Recursion/Combination Sum - 2.cpp
+++ Day 9 - Recursion/Combination Sum - 2.cpp
@@ -43,3 +43,51 @@ int main()
     }
     return 0;
 }
+
+// OPTIMAL =========================================== TC = 2^n * k  , SC = k*x ===================================================================================
+
+// TC = 2^n * k -> where k=average lenght of 'ds' array we are putting into 'ans'
+// SC -> Assume 'x' are the number of combinations possible 
+
+void findCombination(int ind, int target, vector<int> &nums, vector<vector<int>> &ans, vector<int> &ds){
+    // Base - when target becomes '0'
+    if(target == 0){
+        ans.push_back(ds);
+        return;
+    }
+    
+    // loop from current 'index' i.e 'ind' to 'n-1' where n=size of arr
+    for(int i = ind; i < nums.size(); i++){
+        
+        // Check to avoid picking up duplicate elements again
+        // (i > ind) -> checks whether we are visiting the element for the 1st time or not
+        if(i > ind && nums[i] == nums[i-1]) continue;
+        
+        if(nums[i] > target) break;
+        
+        // PICK UP the element
+        ds.push_back(nums[i]);
+        findCombination(i+1, target-nums[i], nums, ans, ds);
+        
+        ds.pop_back();
+    }
+}
+
+int main()
+{
+    vector<int> nums {1, 1, 1, 2, 2};
+    int target = 4;
+    
+    vector<vector<int>> ans;
+    vector<int> ds;
+    
+    findCombination(0, target, nums, ans, ds);
+
+    for(auto it: ans){
+        for(auto num : it){
+            cout<<num<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
