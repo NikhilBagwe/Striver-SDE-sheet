d6a2e8c51d4cf451dcf12cc5c83eabbeb9477d58
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-11-17 13:42:04 +0530

    Update Subset Sum ll.cpp

--- Day 9 - Recursion/Subset Sum ll.cpp
+++ Day 9 - Recursion/Subset Sum ll.cpp
@@ -43,3 +43,43 @@ int main()
 
     return 0;
 }
+
+// BRUTE ================================================ TC = 2^n * n , SC = O(2^n) * O(k) ============================================================================
+
+// TC = 2^n * n -> 2^n for recurssive call and 'n' as every time we are adding a 'ds' to 'ans' which can be of maximum size 'n'
+// SC = O(2^n) * O(k) -> At worst we will store 2^n subsets into 'ans' and their average lenght might be O(k)
+
+void findSubsets(int ind, vector<int> &nums, vector<int> &ds, vector<vector<int>> &ans) {
+        ans.push_back(ds);
+        
+        for(int i = ind;i<nums.size();i++) {
+            // We have to pickup the element only once so we perform this check
+            if(i!=ind && nums[i] == nums[i-1]) continue;
+            
+            ds.push_back(nums[i]); 
+            findSubsets(i+1, nums, ds, ans); 
+            
+            ds.pop_back(); 
+        }
+    }
+int main()
+{
+    vector<int> nums {1, 2, 2, 2, 3, 3};
+    int n = nums.size();
+    
+    // Store unqiue subsets only
+    vector<vector<int>> ans;
+    
+    // Store the current subset temporarily at time of recursion
+    vector<int> ds;
+    
+    findSubsets(0, nums, ds, ans);
+    for(auto it : ans){
+        for(auto num : it){
+            cout<<num<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
