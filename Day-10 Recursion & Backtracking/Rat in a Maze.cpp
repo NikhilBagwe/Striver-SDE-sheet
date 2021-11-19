bcb244b639078acddf350a39045ad50ead02f57e
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-11-19 18:53:36 +0530

    1

--- Day-10 Recursion & Backtracking/Rat in a Maze.cpp
+++ Day-10 Recursion & Backtracking/Rat in a Maze.cpp
@@ -75,3 +75,73 @@ int main()
     }
     return 0;
 }
+
+// OPTIMAL - CLEAN CODE ========================== TC = 4 ^ (n*m) , sc = O(n*m)===================================================================================
+
+/*
+
+LOGIC: 
+
+1. Instead of using 4 'if' statements we can use two arrays which we will use to store the directions.
+
+2. In each 'if' statement , either 1 is added or subtracted from i or j so 
+   As per D - L - R - U -> draw diagram of directions to understand better
+   di[] = [1, 0, 0, -1]
+   dj[] = [0, -1, 1, 0]
+
+*/
+
+void findPaths(int i, int j, vector<vector<int>> &a, int n, vector<string> &ans, string path, vector<vector<int>> &vis,
+    int di[], int dj[]){
+    // Base - When you reach the end-point 
+    if(i == n-1 && j == n-1){
+        ans.push_back(path);
+        return;
+    }
+    
+    string dir = "DLRU";
+    
+    for(int ind = 0; ind < 4; ind++){
+        int nexti = i + di[ind];
+        int nextj = j + dj[ind];
+        
+        // Do all the boundary checks at once
+        if(nexti >= 0 && nextj >= 0 && nexti < n && nextj < n && !vis[nexti][nextj] && a[nexti][nextj] == 1){
+            vis[i][j] = 1;
+            findPaths(nexti, nextj, a, n, ans, path + dir[ind], vis, di, dj);
+            vis[i][j] = 0;
+        } 
+    }
+}
+
+int main()
+{
+    vector<vector<int>> maze {{1,0,0,0}, 
+                              {1,1,0,0},            
+                              {1,1,0,0}, 
+                              {0,1,1,1}};
+    
+    int n = maze.size();
+    // vector string to store paths 
+    vector<string> ans;
+    
+    // Visited array 
+    vector<vector<int>> vis(n, vector<int>(n, 0));
+    
+    // directions array 
+    int di[] = {1, 0, 0, -1};
+    int dj[] = {0, -1, 1, 0};
+    
+    // If the starting point is valid i.e '1' than only we can start traversing the maze
+    if(maze[0][0] == 1){
+        findPaths(0, 0, maze, n, ans, "", vis, di, dj);
+    }
+    
+    for(auto it : ans){
+        for(auto c : it){
+            cout<<c<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
